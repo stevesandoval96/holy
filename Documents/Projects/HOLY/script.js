7c2923a7292e6b3c2ce1131dad70f96f6efde2e3
Steve Sandoval <sandoval_steve96@yahoo.com>
2023-08-10 23:16:48 -0500

    Initial arrays

---
+++ Documents/Projects/HOLY/script.js
@@ -0,0 +1,80 @@
+// All choosable categories
+const marriageWomen = [
+    ["Wives, be subordinate to your husbands as to the Lord. For the husband is head of his wife just as Christ is head of the church, he himself the savior of the body. As the church is subordinate to Christ, so wives should be subordinate to their husbands in everything.", 'Ephesians 5:22-24'],
+    ['Wives, be subject to your husbands, as is fitting in the Lord.', 'Colossians 3:18'],
+    ['Likewise you wives, be submissive to your husbands, so that some, though they do not obey the word, may be won without a word by the behavior of their wives, when they see your reverent and chaste behavior.', '1 Peter 3:1-2'],
+    ['For this is how the holy women who hoped in God used to adorn themselves, by submitting to their own husbands', '1 Peter 3:5'],
+    ['Who can find a good wife? She is far more precious than jewels. The heart of her husband trusts in her, and he will have no lack of gain. She does him good, and not harm, all the days of her life.', 'Proverbs 31:10-12'],
+    ['She opens her mouth with wisdom, and the teachings of kindness is on her tongue.', 'Proverbs 31:26'],
+    ['A good wife is the crown of her husband, but she who causes shame is like rottenness in his bones.', 'Proverbs 12:4'],
+    ['Wisdom builds her house, but Folly tears hers down with her own hands.', 'Proverbs 14:1'],
+    ['She looks well to the ways of her household, and does not eat the bread of idleness. Her children arise and call her blessed; her husband also, and he praises her: "Many women have done excellently, but you surpass them all."', 'Proverbs 31:27-29'],
+    ['but because of cases of immorality every man should have his own wife, and every woman her own husband. The husband should fulfill his duty toward his wife, and likewise the wife toward her husband. A wife does not have authority over her own body, but rather her husband, and similarly a husband does not have authority over his own body, but rather his wife. Do not deprive each other, except perhaps by mutual consent for a time, to be free for prayer, but then return to one another, so that Satan may not tempt you through your lack of self-control.', '1 Corinthians 7:2-5'],
+    ['But I want you to understand that the head of every man is Christ, the head of a wife is her husband, and the head of Christ is God.', '1 Corinthians 11:3'],
+]
+
+const marriageWomenExplanation = [
+    ['Ephesians 5:22-24', ''],
+    ['Colossians 3:18', ''],
+    ['1 Peter 3:1-2', ''],
+    ['1 Peter 3:5', 'For this is the way the holy women of the past who put their hope in God used to adorn themselves. They submitted themselves to their own husbands'],
+    ['Proverbs 31:10-12', ''],
+    ['Proverbs 31:26', ''],
+    ['Proverbs 12:4', ''],
+    ['Proverbs 14:1', ''],
+    ['Proverbs 31:27-29', ''],
+    ['1 Corinthians 7:2-5', ''],
+    ['1 Corinthians 11:3', ''],
+]
+
+
+
+
+
+const marriageMen = [
+    ['however, let each one of you love his wife as himself, and let the wife see that she respects her husband.', 'Ephesians 5:33'],
+    ['Even so husbands should love their wives as their own bodies. He who loves his wife loves himself.', 'Ephesians 5:28'],
+    ['Husbands, love your wives, and do not be harsh with them.', 'Colossians 3:19'],
+    ['Likewise, you husbands should live with your wives in understanding, showing honor to the weaker female sex, since we are joint heirs of the gift of life, so that your prayers may not be hindered.', '1 Peter 3:7'],
+    ['He who finds a wife finds a good thing, and obtains favor from the LORD.', 'Proverbs 18:22'],
+    ['House and wealth are inherited from fathers, but a prudent wife is from the LORD', 'Proverbs 19:14'],
+]
+
+const marriageMenExplanation = [
+    ['Ephesians 5:33', ''],
+    ['Ephesians 5:28', ''],
+    ['Colossians 3:19', ''],
+    ['1 Peter 3:7', ''],
+    ['Proverbs 18:22', 'whoever finds a good wife or husband obtains God\'s grace and blessing. Every person wants such a happy marriage. But whether or not we can obtain is not a matter of individual choice. It depends on God\'s ordination and his sovereignty.'],
+    ['Proverbs 19:14', ''],
+]
+
+
+
+
+
+const marriage = [
+    ['Above all, let your love for one another be intense, because love covers a multitude of sins. Be hospitable to one another without complaining.', '1 Peter 4:8-9'],
+    ['To the married I give this charge (not I, but the Lord): the wife should not separate from her husband (but if she does, she should remain unmarried or else be reconciled to her husband), and the husband should not divorce his wife.', '1 Corinthians 7:10-11'],
+    ['Love is patient and kind; love does not envy or boast; it is not arrogant or rude. It does not insist on its own way; it is not irritable or resentful; it does not rejoice at wrongdoing, but rejoices with the truth. Love bears all things, believes all things, hopes all things, endures all things.', '1 Corinthians 13:4-7'],
+]
+
+const marriageExplanation = [
+    ['1 Peter 4:8-9', 'Above all, love each other deeply, because love covers over a multitude of sins. Offer hospitality to one another without grumbling.'],
+    ['1 Corinthians 7:10-11', ''],
+    ['1 Corinthians 13:4-7', ''],
+]
+
+
+
+
+
+const women = [
+    ['Your adornment should not be an external one: braiding the hair, wearing gold jewelry, or dressing in fine clothes, but rather the hidden character of the heart, expressed in the imperishable beauty of a gentle and calm disposition, which is precious in the sight of God.', '1 Peter 3:3-4'],
+    ['Charm is deceitful, and beauty is vain, but a woman who fears the LORD is to be praised.', 'Proverbs 31:30'],
+]
+
+const womenExplanation = [
+    ['1 Peter 3:3-4', ''],
+    ['Proverbs 31:30', ''],
+]
