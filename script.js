b5ac3ec8b9b86ff92125c11621ea24a65e6e2242
Steve Sandoval <103806644+stevesandoval96@users.noreply.github.com>
2023-08-25 07:58:01 -0500

    Changed Structure: Arrays -> Objects

---
+++ script.js
@@ -0,0 +1,179 @@
+// All choosable categories
+const bible = {
+    verses: [
+        //Marriage -> Pertaining to Women
+        {
+            category: 'marriageWomen',
+            chapter: 'Ephesians 5:22-24',
+            verse: 'Wives, be subordinate to your husbands as to the Lord. For the husband is head of his wife just as Christ is head of the church, he himself the savior of the body. As the church is subordinate to Christ, so wives should be subordinate to their husbands in everything.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: 'Colossians 3:18',
+            verse: 'Wives, be subject to your husbands, as is fitting in the Lord.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: '1 Peter 3:1-2',
+            verse: 'Likewise you wives, be submissive to your husbands, so that some, though they do not obey the word, may be won without a word by the behavior of their wives, when they see your reverent and chaste behavior.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: '1 Peter 3:5',
+            verse: 'For this is how the holy women who hoped in God used to adorn themselves, by submitting to their own husbands',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: 'Proverbs 31:10-12',
+            verse: 'Who can find a good wife? She is far more precious than jewels. The heart of her husband trusts in her, and he will have no lack of gain. She does him good, and not harm, all the days of her life.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: 'Proverbs 31:26',
+            verse: 'She opens her mouth with wisdom, and the teachings of kindness is on her tongue.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: 'Proverbs 12:4',
+            verse: 'A good wife is the crown of her husband, but she who causes shame is like rottenness in his bones.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: 'Proverbs 14:1',
+            verse: 'Wisdom builds her house, but Folly tears hers down with her own hands.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: '1 Corinthians 7:2-5',
+            verse: 'but because of cases of immorality every man should have his own wife, and every woman her own husband. The husband should fulfill his duty toward his wife, and likewise the wife toward her husband. A wife does not have authority over her own body, but rather her husband, and similarly a husband does not have authority over his own body, but rather his wife. Do not deprive each other, except perhaps by mutual consent for a time, to be free for prayer, but then return to one another, so that Satan may not tempt you through your lack of self-control.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: '1 Corinthians 11:3',
+            verse: 'But I want you to understand that the head of every man is Christ, the head of a wife is her husband, and the head of Christ is God.',
+            explanation: ''
+        },
+        {
+            category: 'marriageWomen',
+            chapter: 'COLOSSIANS',
+            verse: 'WOMEN',
+            explanation: ''
+        },
+        //Marriage -> Pertaining to Men 
+        {
+            category: 'marriageMen',
+            chapter: 'Ephesians 5:33',
+            verse: 'however, let each one of you love his wife as himself, and let the wife see that she respects her husband.',
+            explanation: ''
+        },
+        {
+            category: 'marriageMen',
+            chapter: 'Ephesians 5:28',
+            verse: 'Even so husbands should love their wives as their own bodies. He who loves his wife loves himself.',
+            explanation: ''
+        },
+        {
+            category: 'marriageMen',
+            chapter: 'Colossians 3:19',
+            verse: 'Husbands, love your wives, and do not be harsh with them.',
+            explanation: ''
+        },
+        {
+            category: 'marriageMen',
+            chapter: '1 Peter 3:7',
+            verse: 'Likewise, you husbands should live with your wives in understanding, showing honor to the weaker female sex, since we are joint heirs of the gift of life, so that your prayers may not be hindered.',
+            explanation: ''
+        },
+        {
+            category: 'marriageMen',
+            chapter: 'Proverbs 18:22',
+            verse: 'He who finds a wife finds a good thing, and obtains favor from the LORD.',
+            explanation: ''
+        },
+        {
+            category: 'marriageMen',
+            chapter: 'Proverbs 19:14',
+            verse: 'House and wealth are inherited from fathers, but a prudent wife is from the LORD',
+            explanation: ''
+        },
+        {
+            category: 'marriageMen',
+            chapter: 'PSALMS',
+            verse: 'MEN',
+            explanation: ''
+        },
+        //Marriage -> General Verses
+        {
+            category: 'marriage',
+            chapter: '1 Corinthians 7:10-11',
+            verse: 'To the married I give this charge (not I, but the Lord): the wife should not separate from her husband (but if she does, she should remain unmarried or else be reconciled to her husband), and the husband should not divorce his wife.',
+            explanation: ''
+        },
+        {
+            category: 'marriage',
+            chapter: '1 Peter 4:8-9',
+            verse: 'Above all, let your love for one another be intense, because love covers a multitude of sins. Be hospitable to one another without complaining.',
+            explanation: ''
+        },
+        {
+            category: 'marriage',
+            chapter: '1 Corinthians 13:4-7',
+            verse: 'Love is patient and kind; love does not envy or boast; it is not arrogant or rude. It does not insist on its own way; it is not irritable or resentful; it does not rejoice at wrongdoing, but rejoices with the truth. Love bears all things, believes all things, hopes all things, endures all things.',
+            explanation: ''
+        },
+        {
+            category: 'marriage',
+            chapter: 'PSALMS',
+            verse: 'MARRIAGESHOULDBE',
+            explanation: ''
+        },
+        {
+            category: 'marriage',
+            chapter: 'PSALMS',
+            verse: 'MARRIAGESHOULDBE',
+            explanation: ''
+        },
+        //Women -> How to be a Woman of God
+        {
+            category: 'woman',
+            chapter: '1 Peter 3:3-4',
+            verse: 'Your adornment should not be an external one: braiding the hair, wearing gold jewelry, or dressing in fine clothes, but rather the hidden character of the heart, expressed in the imperishable beauty of a gentle and calm disposition, which is precious in the sight of God.',
+            explanation: ''
+        },
+        {
+            category: 'woman',
+            chapter: 'Proverbs 31:30',
+            verse: 'Charm is deceitful, and beauty is vain, but a woman who fears the LORD is to be praised.',
+            explanation: ''
+        },
+        {
+            category: 'woman',
+            chapter: 'PSALMS',
+            verse: 'WOMANshould',
+            explanation: ''
+        },
+        {
+            category: 'woman',
+            chapter: 'PSALMS',
+            verse: 'WOMANshould',
+            explanation: ''
+        },
+    ],
+    prayers: [
+        {
+            category: [
+                {marriageWomen: ['firstPrayer', 'secondPrayer', 'third prayer']},
+                {marriageMen: ['menPrayerOne', 'menPrayerTwo', 'menPrayerThree']}
+            ]
+        }
+    ]
+}
